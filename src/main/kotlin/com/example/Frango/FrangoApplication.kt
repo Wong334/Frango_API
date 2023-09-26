c93668c0850b449b57e1f4ed9e94fc798cfa2477
Wong334 <begomes2004@gmail.com>
2023-09-26 13:54:15 -0300

    iniciando o projeto

---
+++ src/main/kotlin/com/example/Frango/FrangoApplication.kt
@@ -0,0 +1,11 @@
+package com.example.Frango
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FrangoApplication
+
+fun main(args: Array<String>) {
+	runApplication<FrangoApplication>(*args)
+}
